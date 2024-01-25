8bbf78d2b6bf60cdf80e56c1e7607b0b8287f948
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-01-25 10:44:47 +0530

    add: install firebase to project

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMEgIBdha6Ehko8tog42zRjV4kCnaA7r4",
+  authDomain: "hackathon-prep-a75e8.firebaseapp.com",
+  projectId: "hackathon-prep-a75e8",
+  storageBucket: "hackathon-prep-a75e8.appspot.com",
+  messagingSenderId: "903919835009",
+  appId: "1:903919835009:web:bff589b04568b2e9fcfa80",
+  measurementId: "G-M35YDKS2J4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
